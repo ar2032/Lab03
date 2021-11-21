4a214b7417feec14875fab429afd879765112e2e
Anshul <zandar2610@gmail.com>
2021-11-21 07:44:08 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+//creating app
+const app = express();
+
+//handling static HTML and EJS templates
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+ res.render('index'); //no need for ejs extension
+});
+
+//route for contacts
+app.get('/contacts', (req, res) => {
+    res.render('contact');
+});
+
+const router = require('./routes/apis');
+app.use(router);
+
+//routes for dynamic processing of clients
+//-----------------------------------------------
+//route for registration
+router.post('/api/register', clientController.registerControl);
+//route for login
+router.post('/api/login', clientController.loginControl);
+
+
+// //make the app listen on port
+const port = process.argv[2] || process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log(`Cart app listening at http://localhost:${port}`);
+});
+
